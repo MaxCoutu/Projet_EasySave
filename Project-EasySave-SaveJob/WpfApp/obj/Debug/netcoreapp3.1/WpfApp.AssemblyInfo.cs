583b8b36b6728be2dfbe31c53c71d9890dc90654
jérémy maille <jeremy.maille@viacesi.fr>
2025-05-27 16:33:35 +0200

    Update binaries and assembly information for Project-EasySave and WpfApp

--- Project-EasySave-SaveJob/WpfApp/obj/Debug/netcoreapp3.1/WpfApp.AssemblyInfo.cs
+++ Project-EasySave-SaveJob/WpfApp/obj/Debug/netcoreapp3.1/WpfApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5649f10635611291985fcd95d69486c1c9152158")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9532818a21f8f72750808ab4a2b56417713695cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
